5401874415380c15884566c64af3768c5cf22bce
mtdemers <mtdemers@github.com>
2017-06-16 10:10:47 -0700

    Add in beautiful soup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # puller
+This is intended to scrape RSS feeds and process into a database based on key words used in the posts.
+
--- puller.py
+++ puller.py
@@ -1,18 +1,18 @@
 #Web scraper and sorter
 #For personal, non-profit use
+from bs4 import BeautifulSoup #HTML parsing
+from requests import get #HTML requests
 
-from requests import get
+def scrape(s):
+    str(s) #change site to string
+    r = get(s) #scrape site data
+    data = r.text #change site data to text
+    soup = BeautifulSoup(data, "html.parser")  #parse text
+    print soup #display parsed text
 
-def input(s):
-    str(s)
-    r = get(s)
-    print r.text
-    return 
 
-site = raw_input("What site are we scraping? ")
+site = raw_input("What site are we scraping? ") #input site
 
 print "Retrieving data for %s" % site
 
-input(site)
-
-
+scrape(site) #pass site to scrape
